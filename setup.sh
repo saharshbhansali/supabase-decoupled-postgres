17b922a7cd7b70781a11e0a503bb49c4b4b5c946
saharshbhansali <saharsh.bhansali15@gmail.com>
2024-08-01 08:06:37 +0530

    Update: formatting fixes in scripts
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- setup.sh
+++ setup.sh
@@ -2,22 +2,30 @@
 
 cp .env.example .env
 
-echo "example env copied"
+echo "Example .env copied."
 
 docker network create testnet
 
-echo "Separate docker network created"
+echo "Separate docker network created."
 
 docker compose -f ./docker-compose.externaldb.yml up -d
 
-echo "External Postgres Database Up"
+echo "External Postgres database running."
 
-echo "Sleeping... 15s"
+echo "Sleeping... 15s."
 
 sleep 15s
 
-echo "Starting other services"
+echo "Starting other services:"
 
 docker compose -f ./docker-compose.yml up -d
 
-echo "Done!"
+echo "Done\!"
+
+echo "Run:"
+
+echo "docker compose -f ./docker-compose.yml down && docker compose -f ./docker-compose.externaldb.yml down"
+
+echo "to stop the active docker services"
+
+echo "Debug using: docker compose [attach|logs] <service-name>"
