da517c980624f47f5bdfc66265ba50cf3be8d7ad
saharshbhansali <saharsh.bhansali15@gmail.com>
2024-08-01 07:54:31 +0530

    Update: setup script - added comment echoes
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+cp .env.example .env
+
+echo "example env copied"
+
 docker network create testnet
+
+echo "Separate docker network created"
 
 docker compose -f ./docker-compose.externaldb.yml up -d
 
+echo "External Postgres Database Up"
+
+echo "Sleeping... 15s"
+
 sleep 15s
 
+echo "Starting other services"
+
 docker compose -f ./docker-compose.yml up -d
+
+echo "Done!"
